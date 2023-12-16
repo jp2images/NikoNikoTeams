206ed722e09a9ec08daf289acb0212915464cde6
Jeff <jeff_patterson@outlook.com>
2023-12-16 11:23:44 -0600

    Saving current progress. Legend looks good and emojis are back.

---
+++ Entities/NikoNikoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NikoNikoTeams.Entities;
+
+public class NikoNikoItem
+{
+  public string ImageSource { get; set; }
+  public string ImageName { get; set; }
+  public int Score { get; set; }
+  public string Definition { get; set; }
+}
